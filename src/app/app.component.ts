81f76278c13da506d5890f0bd68bc8ce4b144779
torres10luis <Luis.angel_15@yahoo.com>
2018-06-21 15:24:38 -0700

    practice 1 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  bools: any[] = [true, false];
+  nums: [1, 2.5, 5];
+  strs: ['hi', 'there'];
+
+  test: [{
+    id: 1, name: 'test'
+  }];
+
+  user: [{
+    id: 2, name: 'user'
+  }];
+
+  objs: Array<object> = [this.test, this.user];
+
+  types: Array<any> = [this.bools, this.nums, this.strs, this.objs];
+
+  getTypeOf(types) {
+    return typeof types;
+  }
 }
+
+
+
